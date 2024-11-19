1539de380ed3b22ce5f0a67074576097294f0be8
Gabriel Matioli <gabrielmatioli@Gabriels-MacBook-Air.local>
2024-11-18 21:49:42 -0700

    new stuff

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import HtmlPlugin from 'vite-plugin-html';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    HtmlPlugin({
+      inject: {
+        injectHtml: `
+          <base href="/task-app-react/" />
+        `
+      }
+    })
+  ]
+});
